dfd41bb53789fb4c52c942c11f09a84a451fa2a9
Duy <duy@kvikymart.cz>
2019-05-25 12:33:17 +0200

    Render Ship item differently

--- ts/OrderedItem.ts
+++ ts/OrderedItem.ts
@@ -4,11 +4,14 @@ export class OrderedItem {
     public constructor(private item: Item, private quantity: number) {
     }
 
-    public getPriceAmount():number {
+    public getPriceAmount(): number {
         return this.item.pricePerUnit * this.quantity;
     }
 
     public getLineOnBill(): string {
+        if (this.item.name.toLowerCase() === 'ship') {
+            return this.item.name;
+        }
         return `${this.item.name} (${this.quantity}${this.item.unitName} x ${this.item.pricePerUnit}k)`;
     }
 
